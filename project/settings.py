28ffd66bfb33729222df1267bbd7a371c7c886a5
lrbecerrab <70267843+lrbecerrab@users.noreply.github.com>
2020-12-12 10:43:08 -0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'categories_fragen.apps.FragerConfig'
+    'categories_fragen.apps.FragenConfig'
 ]
 
 MIDDLEWARE = [
